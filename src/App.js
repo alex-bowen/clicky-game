43342fecf3f8aa65c1755356721eed4a5a294f42
alex-bowen <alexbowen2017@gmail.com>
2018-10-26 21:21:35 -0500

    styled and static

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CharacterCard from "./components/CharacterCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Counter from "./components/Counter";
+import characters from "./characters.json";
+
+import "./App.css";
+
 
 class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    characters
+  };
+
+  handleClick = id => {
+
+    console.log(id)
+  
+  }
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        <Title>Parks &amp; Rec Clicky Game!
+          <hr />
+          <p className="p">Don't click on anyone twice! Click on any character to begin. Characters will shuffle after each click.</p>
+          <br />
+          <Counter />
+        </Title>
+        {this.state.characters.map(character => (
+          <CharacterCard
+            id={this.id}
+            image={character.image}
+            handleClick={this.handleClick}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
